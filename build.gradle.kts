a8b6497a9e47fb09d8c7d1417dabbad5cce548c5
Leon Linhart <themrmilchmann@gmail.com>
2022-01-19 13:21:18 +0100

    build: add build scripts

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2022 Leon Linhart
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+plugins {
+    groovy
+    `kotlin-dsl`
+    kotlin("plugin.serialization") version "1.5.31"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "com.github.themrmilchmann.gradle.curseforge.publish"
+val nextVersion = "0.1.0"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
+}
+
+kotlin {
+    explicitApi()
+}
+
+gradlePlugin {
+    plugins {
+        create("curseForgePublish") {
+            id = "com.github.themrmilchmann.curseforge-publish"
+            implementationClass = "com.github.themrmilchmann.gradle.publish.curseforge.plugins.CurseForgePublishPlugin"
+        }
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    // needed to prevent inclusion of gradle-api into shadow JAR
+    configurations.implementation.get().dependencies.remove(gradleApi())
+
+    shadow(localGroovy())
+    shadow(gradleApi())
+
+    implementation("io.ktor:ktor-client-apache:1.6.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+
+    testImplementation(gradleTestKit())
+    testImplementation(platform("org.spockframework:spock-bom:2.0-groovy-3.0"))
+    testImplementation("org.spockframework:spock-core")
+}
